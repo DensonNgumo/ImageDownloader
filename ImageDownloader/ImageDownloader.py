4b537cfd13c3d91a5c89cfe1dd21a1716d023beb
DensonNgumo <ngumohd@gmail.com>
2016-06-21 13:19:43 +0300

    A working image downloader which gets an image from the web provided its url

---
+++ ImageDownloader/ImageDownloader.py
@@ -0,0 +1,18 @@
+import random
+import urllib.request
+
+def getRandomTitle():
+    randomString='img'
+    alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for n in range(5):
+        randomString+=alphabets[random.randrange(0,25)]
+    randomString+=str(random.randrange(1,1000))+".jpg"    
+    return randomString    
+   
+def downloadImage(url):
+    imageTitle= getRandomTitle()
+    urllib.request.urlretrieve(url,imageTitle)
+    print("Downloaded")
+
+downloadImage("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQl5X3hLq__uUkt6uhJVajsP-F2U8IZ-0WumS12BdOIa4he91HcJnW1cA")
+
